1c0464fa0a04ab5ed9317286d4c76818fb203895
manojgain1988 <manojgain1988@gmail.com>
2023-11-13 21:10:09 +0600

    add scrollbar

---
+++ js/apps.js
@@ -0,0 +1,28 @@
+
+
+
+function topScroll(){
+
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
+
+
+// function  topScroll(){
+//     window.scrollTo({
+//         top:0,
+//         right:0,
+//         behavior:"smooth"
+//     });  
+   
+// };
+
+
+
+// function  topScroll(){
+//     window.scrollTo({
+//         top:0,
+//       right:0,
+//       behavior:"smooth"
+//     })
+// }
