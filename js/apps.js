4517134b7a56f5bc998b5a74eeed253c124f1133
manojgain1988 <manojgain1988@gmail.com>
2023-11-15 20:35:16 +0600

    some theig add

--- js/apps.js
+++ js/apps.js
@@ -1,28 +1,21 @@
 
 
 
-function topScroll(){
+// function topScroll(){
 
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-};
-
-
-// function  topScroll(){
-//     window.scrollTo({
-//         top:0,
-//         right:0,
-//         behavior:"smooth"
-//     });  
-   
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
 // };
 
 
+function  topScroll(){
+    window.scrollTo({
+        top:0,
+        right:0,
+        behavior:"smooth"
+    });  
+   
+};
 
-// function  topScroll(){
-//     window.scrollTo({
-//         top:0,
-//       right:0,
-//       behavior:"smooth"
-//     })
-// }
+
+
